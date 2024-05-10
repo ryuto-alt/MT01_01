d6772411666fd74047d2582fe6b467184df586f3
ryuto-alt <k023g0078@g.neec.ac.jp>
2024-05-10 16:36:58 +0900

    初期状態

--- MT1_01/main.cpp
+++ MT1_01/main.cpp
@@ -1,24 +1,12 @@
 #include <Novice.h>
-#include"Matrix.h"
 
 const char kWindowTitle[] = "LE2C_05_ウノ_リュウト";
-int kWindowWidth = 1280;
-int kWindowHeight = 720;
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	Vector3 v1{ 1.2f,-3.9f,2.5f };
-	Vector3 v2{ 2.8f,0.4f,-1.3f };
-	Vector3 cross = Cross(v1, v2);
-	Vector3 rotate{  };
-	Vector3 translate{ };
-	Vector3 cameraPosition{ 0.0f,0.0f,-10.0f };
-	Vector3 kLocalVertices[3] = {
-	{ 0.0f,1.0f,0.0f } ,
-	{ 1.0f,-1.0f,0.0f } ,
-	{ -1.0f,-1.0f,0.0f }
-	};
+
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -37,36 +25,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		if (keys[DIK_W]) {
-			translate.z++;
-		}
-		if (keys[DIK_S]) {
-			translate.z--;
-		}
-		if (keys[DIK_A]) {
-			translate.x--;
-		}
-		if (keys[DIK_D]) {
-			translate.x++;
-		}
-		rotate.y += 0.03f;
-
-		Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
-		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, cameraPosition);
-		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
-		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
-		Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
-		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
-		Vector3 screenVertices[3];
-		for (uint32_t i = 0; i < 3; ++i) {
-			Vector3 ndcVertex = Transform(kLocalVertices[i], worldViewProjectionMatrix);
-			screenVertices[i] = Transform(ndcVertex, viewportMatrix);
-		}
-		Vector3 cross1 = Subtract(screenVertices[1], screenVertices[0]);
-		Vector3 cross2 = Subtract(screenVertices[2], screenVertices[1]);
-		Vector3 cross3 = Cross(cross1, cross2);
-		float dot = Dot(cameraPosition, cross3);
-
 		///
 		/// ↑更新処理ここまで
 		///
@@ -74,15 +32,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		VectorScreenPrintf(0, 0, cross);
-		if (dot < 0) {
 
-
-			Novice::DrawTriangle(
-				int(screenVertices[0].x), int(screenVertices[0].y), int(screenVertices[1].x), int(screenVertices[1].y),
-				int(screenVertices[2].x), int(screenVertices[2].y), RED, kFillModeSolid
-			);
-		}
 		///
 		/// ↑描画処理ここまで
 		///
