97c5f7aeb9f00ef22b88e12be577b34cd849a100
ryuto-alt <k023g0078@g.neec.ac.jp>
2024-06-21 15:00:04 +0900

    02_03

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -1,3 +1,4 @@
+
 #include <Novice.h>
 #include"Matrix.h"
 #include <math.h>
@@ -7,25 +8,26 @@ const char kWindowTitle[] = "LE2C_05_ウノ_リュウト";
 int kWindowWidth = 1280;
 int kWindowHeight = 720;
 struct Sphere {
-	Vector3 center; 
-	float radius;   
+	Vector3 center; // !< 中心点
+	float radius;   // !< 半径
 };
 struct Line {
-	Vector3 origin; 
-	Vector3 diff;   
+	Vector3 origin; // !<始点
+	Vector3 diff;   // !<終点への差分ベクトル
 };
 struct Ray {
-	Vector3 origin; 
-	Vector3 diff;   
+	Vector3 origin; // !<始点
+	Vector3 diff;   // !<終点への差分ベクトル
 };
 struct Segment {
-	Vector3 origin; 
-	Vector3 diff;   
+	Vector3 origin; // !<始点
+	Vector3 diff;   // !<終点への差分ベクトル
 };
 struct Plane {
-	Vector3 normal; 
-	float distance; 
+	Vector3 normal; //!< 法線
+	float distance; //!< 距離
 };
+
 void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix) {
 	const float kGridHalfWidth = 2.0f;                                      // グリッドの半分の幅
 	const uint32_t kSubdivision = 10;                                       // 分割数
@@ -146,18 +148,39 @@ bool IsCollision(const Sphere& s1, const Sphere& s2) {
 	// 中心間の距離が2つの円の半径の合計よりも小さい場合、衝突しているとみなす
 	if (distance <= (s1.radius + s2.radius)) {
 		return true;
-	} else {
+	}
+	else {
 		return false;
 	}
 }
 bool IsCollision(const Sphere& sphere, const Plane& plane) {
 	// 2つの円と平面の中心間の距離を計算
-	float distance = std::abs((Dot(plane.normal, sphere.center))-plane.distance);
+	float distance = std::abs((Dot(plane.normal, sphere.center)) - plane.distance);
 	//float distance = float(std::sqrt(std::pow(plane.normal.x - sphere.center.x, 2) + std::pow(plane.normal.y - sphere.center.y, 2) + std::pow(plane.normal.z - sphere.center.z, 2)));
 	// 中心間の距離が2つの円の半径の合計よりも小さい場合、衝突しているとみなす
 	if (distance <= sphere.radius) {
 		return true;
-	} else {
+	}
+	else {
+		return false;
+	}
+}
+bool IsCollision(const Segment& segment, const Plane& plane) {
+	// まず垂直判定を行うために、法線と線の内積を求める
+	float dot = Dot(plane.normal, segment.diff);
+	// 垂直＝平行であるので、衝突しているはずがない
+	if (dot == 0.0f) {
+		return false;
+	}
+	// ｔを求める
+	float t = (plane.distance - Dot(segment.origin, plane.normal)) / dot;
+
+	// 
+	//float distance = Dot(segment.origin + (segment.diff * t), plane.normal);
+	if (t >= 0.0f && t <= 1.0f) {
+		return true;
+	}
+	else {
 		return false;
 	}
 }
@@ -173,7 +196,7 @@ void DrawPlane(const Plane& plane, const Matrix4x4& viewProjectionMatrix, const 
 	perpendiculars[0] = Normalize(Perpendicular(plane.normal));//2
 	perpendiculars[1] = { -perpendiculars[0].x,-perpendiculars[0].y,-perpendiculars[0].z };//3
 	perpendiculars[2] = Cross(plane.normal, perpendiculars[0]);//4
-	perpendiculars[3] = { -perpendiculars[2].x ,- perpendiculars[2].y,-perpendiculars[2].z };//5
+	perpendiculars[3] = { -perpendiculars[2].x ,-perpendiculars[2].y,-perpendiculars[2].z };//5
 	//6
 	Vector3 points[4];
 	for (int32_t index = 0; index < 4; ++index) {
@@ -189,7 +212,7 @@ void DrawPlane(const Plane& plane, const Matrix4x4& viewProjectionMatrix, const 
 }
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	
+
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
@@ -198,18 +221,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 cameraPosition{ 0.0f, 1.0f, -5.0f };
 	//const int kWindowWidth = 1280;
 	//const int kWindowHeight = 720;
-	Sphere sphere;
+	/*Sphere sphere;
 	sphere.center = { 0, 0, 0 };
-	sphere.radius = 1;
+	sphere.radius = 1;*/
 	Plane plane;
-	plane.normal={ 0.0f,1.0f,0.0f };
+	plane.normal = { 0.0f,1.0f,0.0f };
 	plane.distance = 1.0f;
 	int planeColor = 0;
-	
+
 	Segment segment{ {-2.0f,-1.0f,0.0f},{3.0f,2.0f,2.0f} };
 	Vector3 point{ -1.5f,0.6f,0.6f };
-	Sphere pointSphere{ point,0.01f };
-	
+	/*Sphere pointSphere{ point,0.01f };*/
+	Segment segment2{ {-2.0f,-1.0f,0.0f},{3.0f,2.0f,2.0f} };
 	Vector3 project = Project(Subtract(point, segment.origin), segment.diff);
 	Vector3 closestPoint = ClosestPoint(point, segment);
 	Sphere closestPointSphere{ closestPoint,0.01f };
@@ -217,7 +240,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
-	
+
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -230,9 +253,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-	
 
-		cameraPosition.z+= Novice::GetWheel()/100;
+
+		/*
+		int mouseX = 0, mouseY = 0;
+		if (Novice::IsTriggerMouse(0)) {
+			Novice::GetMousePosition(&mouseX, &mouseY);
+		}
+		if (Novice::IsPressMouse(0)) {
+			static int lastMouseX, lastMouseY;
+			Novice::GetMousePosition(&lastMouseX, &lastMouseY);
+			int dx = mouseX - lastMouseX;
+			int dy = mouseY - lastMouseY;
+			lastMouseX = mouseX;
+			lastMouseY = mouseY;
+			cameraRotate.y += float(dx*0.001f);
+			cameraRotate.x += float(dy * 0.001f);
+			mouseX = lastMouseX;
+			mouseY = lastMouseY;
+			Novice::GetMousePosition(&mouseX, &mouseY);
+		}
+		*/
+
+		cameraPosition.z += Novice::GetWheel() / 100;
 		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, cameraRotate, Add(cameraPosition, cameraTranslate));
 		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
 		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
@@ -240,35 +283,40 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
 		Vector3 start = Transform(Transform(segment.origin, ViewProjectionMatrix), viewportMatrix);
 		Vector3 end = Transform(Transform(Add(segment.origin, segment.diff), ViewProjectionMatrix), viewportMatrix);
-		if (IsCollision(sphere, plane) == true) {
+		Vector3 start2 = Transform(Transform(segment2.origin, ViewProjectionMatrix), viewportMatrix);
+		Vector3 end2 = Transform(Transform(Add(segment2.origin, segment2.diff), ViewProjectionMatrix), viewportMatrix);
+		if (IsCollision(segment2, plane) == true) {
 			planeColor = RED;
-		} else
+		}
+		else
 		{
 			planeColor = WHITE;
 		}
-		
 		///
 		/// ↑更新処理ここまで
 		///
-		
+
 		///
 		/// ↓描画処理ここから
 		///
-		
+
 		ImGui::Begin("Window");
 		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
 		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
 		ImGui::DragFloat3("CameraPosition", &cameraPosition.x, 0.01f);
-		ImGui::DragFloat3("SphereCenter", &sphere.center.x, 0.01f);
-		ImGui::DragFloat("SphereRadius", &sphere.radius, 0.01f);
+		/*ImGui::DragFloat3("SphereCenter", &sphere.center.x, 0.01f);
+		ImGui::DragFloat("SphereRadius", &sphere.radius, 0.01f);*/
 		ImGui::DragFloat3("Plane.Normal", &plane.normal.x, 0.01f);
 		ImGui::DragFloat("PlaneDistance", &plane.distance, 0.01f);
 		plane.normal = Normalize(plane.normal);
 		ImGui::InputFloat3("Project", &project.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
 		ImGui::End();
 		DrawGrid(ViewProjectionMatrix, viewportMatrix);
-		DrawSphere(sphere, ViewProjectionMatrix, viewportMatrix, planeColor);
-		DrawPlane(plane, ViewProjectionMatrix, viewportMatrix, WHITE);
+		/*DrawSphere(sphere, ViewProjectionMatrix, viewportMatrix, planeColor);*/
+		DrawPlane(plane, ViewProjectionMatrix, viewportMatrix, planeColor);
+		//DrawSphere(pointSphere, ViewProjectionMatrix, viewportMatrix, RED);
+		//DrawSphere(closestPointSphere, ViewProjectionMatrix, viewportMatrix, BLACK);
+		Novice::DrawLine(int(start2.x), int(start2.y), int(end2.x), int(end2.y), WHITE);
 		///
 		/// ↑描画処理ここまで
 		///
