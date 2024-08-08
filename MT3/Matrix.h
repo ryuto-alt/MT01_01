78102710209a1d038012349cfb056009f7f30e9b
ryuto-alt <k023g0078@g.neec.ac.jp>
2024-08-09 06:22:24 +0900

    04_03

--- MT3/Matrix.h
+++ MT3/Matrix.h
@@ -41,6 +41,14 @@ struct Pendulum {
 	float angularAcceleration;//角加速度
 };
 
+struct ConicalPendulum {
+	Vector3 anchor;// アンカーポイント。固定された端の位置
+	float length; //紐の長さ
+	float halfApexAngle;// 円錐の頂角の半分
+	float angle; //現在の角度
+	float angularVelocity;//角速度
+};
+
 
 struct Sphere {
 	Vector3 center; // !< 中心点
